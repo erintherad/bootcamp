9fdfcf43c75307789dfff46cf9efb187f0173ba9
Erin Mahoney <erinmahoneyphoto@gmail.com>
2015-08-28 11:14:42 -0700

    Initialized project

---
+++ app.js
@@ -0,0 +1,31 @@
+angular.module('bootcamp', ['ngRoute'])
+
+	.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+		$routeProvider
+		.when('/', {
+			templateUrl: 'templates/searchGif.html',
+			controller: 'MainCtrl'
+		});
+
+		$locationProvider.html5Mode( {
+			enabled: true,
+			requireBase: false
+		});
+	}])
+
+	.controller('MainCtrl', ['$scope', '$http', function($scope, $http) {
+		$scope.gifs = [];
+
+		$scope.searchTag = function() {
+			var tag = $scope.tag.replace(/\s+/, '');
+			var url = 'http://api.giphy.com/v1/gifs/search?q=' + tag + '&api_key=dc6zaTOxFJmzC&callback=JSON_CALLBACK';
+				$http.jsonp(url)
+				.then(function(response) {
+					$scope.tag = '';
+					$scope.gifs = response.data.data;
+				});
+		};
+
+	}])
+
+	;
